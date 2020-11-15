b09ad22b68881e6ace70b992e635b60bf7e3360a
RihardsJ <rihards.jukna@gmail.com>
2020-11-15 09:54:40 +0000

    create chat beckend with socketIO

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const server = express();
 const http = require("http").createServer(server);
 const PORT = process.env.PORT || 3000;
+const login = require("./login");
 
 const path = require("path");
 
@@ -11,6 +12,13 @@ server.use("/public", express.static("public"));
 
 server.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.on("disconnect", () => {
+    console.log("goodbye!");
+  });
 });
 
 http.listen(PORT, () => {
